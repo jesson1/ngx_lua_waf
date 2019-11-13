76e56ba2bea4d8de75a877420262829274396f8b
yujiasheng <yujiasheng@kedacom.com>
2019-11-13 15:05:44 +0800

    修改安装脚本

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ if [ ! -x "v0.2.17rc2.zip" ]; then
 wget https://github.com/simpl/ngx_devel_kit/archive/v0.2.17rc2.zip
 fi
 unzip v0.2.17rc2
-if [ ! -x "v0.7.4.zip" ]; then  
-wget https://github.com/chaoslawful/lua-nginx-module/archive/v0.7.4.zip
+if [ ! -x "v0.10.13.tar.gz" ]; then  
+wget https://github.com/openresty/lua-nginx-module/archive/v0.10.13.tar.gz
 fi
-unzip v0.7.4
+tar zxvf v0.10.13.tar.gz
 cd /data/src
 if [ ! -x "pcre-8.10.tar.gz" ]; then
 wget http://blog.s135.com/soft/linux/nginx_php/pcre/pcre-8.10.tar.gz
@@ -26,11 +26,11 @@ cd pcre-8.10/
 ./configure
 make && make install
 cd ..
-if [ ! -x "nginx-1.2.4.tar.gz" ]; then
-wget 'http://nginx.org/download/nginx-1.2.4.tar.gz'
+if [ ! -x "nginx-1.14.2.tar.gz" ]; then
+wget 'http://nginx.org/download/nginx-1.14.2.tar.gz'
 fi
-tar -xzvf nginx-1.2.4.tar.gz
-cd nginx-1.2.4/
+tar -xzvf nginx-1.14.2.tar.gz
+cd nginx-1.14.2/
 export LUAJIT_LIB=/usr/local/lj2/lib/
 export LUAJIT_INC=/usr/local/lj2/include/luajit-2.0/
 ./configure --user=daemon --group=daemon --prefix=/usr/local/nginx/ --with-http_stub_status_module --with-http_sub_module --with-http_gzip_static_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module  --add-module=../ngx_devel_kit-0.2.17rc2/ --add-module=../lua-nginx-module-0.7.4/
@@ -40,8 +40,8 @@ make install
 cd /usr/local/nginx/conf/
 wget https://github.com/loveshell/ngx_lua_waf/archive/master.zip --no-check-certificate
 unzip master.zip
-mv ngx_lua_waf-master/* /usr/local/nginx/conf/
-rm -rf ngx_lua_waf-master
-rm -rf /data/src
-mkdir -p /data/logs/hack
-chmod -R 775 /data/logs/hack
+mv ngx_lua_waf-master waf
+# rm -rf /data/src
+mkdir -p /usr/local/nginx/logs/hack/
+chmod -R 777 /usr/local/nginx/logs/hack/
+
